3b7fefe284115c0c27ef00b4328bee8d818a460f
Emerson Beltrán <emerson.beltran@gmail.com>
2020-10-01 12:36:13 -0500

    personalizacion para kontigo

--- src/GtmPlugin.js
+++ src/GtmPlugin.js
@@ -75,9 +75,9 @@ export default class AnalyticsPlugin {
       let dataLayer = (window.dataLayer = window.dataLayer || [])
       dataLayer.push({
         event: event || 'interaction',
-        target: category,
+        category,
         action: action,
-        'target-properties': label,
+        label,
         value: value,
         'interaction-type': noninteraction,
         ...rest
